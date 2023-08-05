d0f9aff750199c63ee5543adc620be38798d8db4
Darren Wong <me@darrenwong.id.au>
2023-08-05 11:29:21 +1000

    UPD 202207 Int

--- 2022/07-no-space-left-on-device.R
+++ 2022/07-no-space-left-on-device.R
@@ -44,16 +44,18 @@ system_commands <- readLines(day_7_input_fp)
 #  For every ls, populate the current working folder with the contents
 
 command_crawler <- function(
-    commands, 
-    symbols = list(
-      dir_sep = "/",
-      user_input = "$", 
-      change_dir = "cd", 
-      list_dir_contents = "ls",
-      dir_prefix = "dir",
-      file_prefix = "[0-9]+",
-      updir = '..'
-    )) {
+  commands, 
+  symbols = list(
+    dir_sep = "/",
+    user_input = "$", 
+    change_dir = "cd", 
+    list_dir_contents = "ls",
+    dir_prefix = "dir",
+    file_prefix = "[0-9]+",
+    updir = '..'
+  ),
+  verbose = FALSE
+) {
   
   #' Can't just extract all `$ cd`s, in case there is a directory found via 
   #' `$ ls` that is not then `cd`d into by the user
@@ -128,30 +130,36 @@ command_crawler <- function(
   
   for (i in 1:length(commands)) {
     
+    if (verbose) cat('Parsing command', i, '\n')
+    
     #' If the command is to change directory...
     if (str_detect(commands[[i]], fixed(paste(symbols$user_input, symbols$change_dir)))) {
       
-      # PULL INTO SEPARATE FUNCTION
+      # PULL INTO SEPARATE FUNCTION? Too many args probably
       
       #' Get directory we're changing to
       dir_target <- str_remove(commands[[i]], fixed('$ cd '))
       
       #' Find where we are in the directory tree
-      current_directory <- case_when(
-        
+      #'   Can't use `case_when()` since it recycles RHS to the largest RHS
+      #'   output supplied. Use if-else instead.
+      current_directory <- if (
         #' If we're navigating to the home directory - move us back there
-        dir_target == symbols$dir_sep ~ c('~'),
-        
+        dir_target == symbols$dir_sep
+      ) {
+        '~'
+      } else if (
         #' Else if the path is an absolute path (starts with a '/') then build
         #' the full path, splitting by '/'
-        str_sub(dir_target, 1, 1) == symbols$dir_sep ~ str_split_1(dir_target, '/') %>% 
+        str_sub(dir_target, 1, 1) == symbols$dir_sep & nchar(dir_target) > 1
+      ) {
+        str_split_1(dir_target, '/') %>% 
           `[`(2:length(.)) %>% 
-          c('~', .),
-        
+          c('~', .)
+      } else (
         #' Otherwise, this will be a relative directory movement. Add the
         #' target directory to the current directory.
-        TRUE ~ c(current_directory, dir_target)
-        
+        c(current_directory, dir_target)
       ) %>% 
         #' Then move up a directory for every '..' present
         handle_updir()
@@ -159,9 +167,12 @@ command_crawler <- function(
       #' Each time the current directory is updated, see if that directory,
       #' and the levels leading to it, are in the `file_system` list. If it
       #' doesn't exist, add it.
-      if (is.null(Reduce(`[[`, current_directory %>% `[`(2:length(.)), init = dir_tree))) {
+      if (
+        length(current_directory) > 1 &
+          is.null(Reduce(`[[`, current_directory %>% `[`(2:length(.)), init = dir_tree))
+      ) {
         
-        #' Need a way to modify arbitrary levels of a list
+        #' Recursively create new directories for each non-existent path
         dir_tree <- recursive_add(dir_tree, current_directory)
         
       }
@@ -189,9 +200,10 @@ command_crawler <- function(
     
   }
   
-}
+} 
 
-command_crawler(system_commands)
+
+command_crawler(system_commands, verbose = T)
 
 # later, create a lookup table with file sizes and then use that to sum???
 
